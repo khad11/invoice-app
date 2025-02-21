1cc7a667e9171e488067fcc7eac05a1ab13e9edf
khadir <tolibxonovm@gmail.com>
2025-02-21 17:57:31 +0500

    initialize React app with Vite, Tailwind CSS, and ESLint configuration

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import SideBar from "./components/SideBar";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return <SideBar />;
+}
+
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,30 @@
+import { MoonIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import LogoImage from "/public/logo.svg";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+function SideBar() {
+  return (
+    <div className="">
+      <div className="flex h-full items-center justify-between bg-slate-700 shadow-lg">
+        <img
+          className="h-20 w-20 object-contain"
+          src={LogoImage}
+          alt="logo image"
+        />
+        <div className="mr-5 flex h-full items-center gap-5">
+          <Button variant="outline" size="icon">
+            <MoonIcon />
+          </Button>
+
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default SideBar;
